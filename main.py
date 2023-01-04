ba745d764f15fd4f08f367db1c64be20c37af93d
Diego Farias <diego.farias.fernandes@gmail.com>
2023-01-04 16:02:33 -0300

    Criada função lucros e prejuízos

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import requests
 import json
 from colorama import Fore
+from http import HTTPStatus
 
 
-ticker = input('Ticker: ')
+def lucros_prejuizos(ticker):
+    url = f'https://brapi.dev/api/quote/{ticker}'
+    response = requests.get(url)
+    data = json.loads(response.content)
 
-url = f'https://brapi.dev/api/quote/{ticker}'
-response = requests.get(url)
-data = json.loads(response.content)
-
-if response.status_code == 200:
-    preco_compra = 21
-    quantidade = 10
-    data_market_price = data['results'][0]['regularMarketPrice']
-    lucro_prejuizo = round(preco_compra - data_market_price, 2) * quantidade
-    
-    if 'longName' not in data['results'][0]:
-        print(data['results'][0]['shortName'])
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        print('Página não encontrada')
     else:
-        print(data['results'][0]['longName'])
+        preco_compra = 18.1
+        quantidade = 10
+        data_market_price = data['results'][0]['regularMarketPrice']
+        lucro_prejuizo = round(preco_compra - data_market_price, 2) * quantidade
     
-    print(data['results'][0]['longName'])
-    print('R$ ', data_market_price)
-    if preco_compra > data_market_price:
-        print(Fore.RED + f'Prejuízo: - R$ {str(lucro_prejuizo)}')
-    else:
-        print(Fore.GREEN + f'Lucro: + R$ {str(lucro_prejuizo).replace("-", "")}')
-else:
-    print('Página não encontrada')
+        if 'longName' not in data['results'][0]:
+            print(data['results'][0]['shortName'])
+        else:
+            print(data['results'][0]['longName'])
+            
+        print('R$ ', data_market_price)
+        if preco_compra > data_market_price:
+            print(Fore.RED + f'Prejuízo: R$ {str(lucro_prejuizo)}')
+        else:
+            print(Fore.GREEN + f'Lucro: R$ {str(lucro_prejuizo).replace("-", "")}')
+
+
+lucros_prejuizos('MXRF11')
