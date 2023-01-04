3f68e4bd4c4edf4cff44ebb83c3da3f64c246600
Diego Farias <diego.farias.fernandes@gmail.com>
2023-01-04 13:45:42 -0300

    Exemplo de consumo da API de cotações da  bolsa

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+ticker = input('Ticker: ')
+
+url = f'https://brapi.dev/api/quote/{ticker}'
+response = requests.get(url)
+data = json.loads(response.content)
+
+if response.status_code == 200:
+    preco_compra = 21
+    quantidade = 10
+    data_market_price = data['results'][0]['regularMarketPrice']
+    lucro_prejuizo = round(preco_compra - data_market_price, 2) * quantidade
+    
+    print(data['results'][0]['longName'])
+    print('R$ ', data_market_price)
+    if preco_compra > data_market_price:
+        print(f'Prejuízo: - R$ {str(lucro_prejuizo)}')
+    else:
+        print(f'Lucro: + R$ {str(lucro_prejuizo).replace("-", "")}')
+else:
+    print('Página não encontrada')
